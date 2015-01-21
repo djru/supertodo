e64f5e65ede527586934561e5a849dfc4a0a2301
Dan Ruswick <Dan@dhcp-10-101-140-170.wireless.northwestern.private>
2015-01-21 12:25:58 -0600

    security fixes

--- simple-todo.js
+++ simple-todo.js
@@ -2,27 +2,26 @@ Items = new Mongo.Collection("items");
 
 Meteor.methods({
 	removeCompleted: function(){ Items.remove({checked: true, owner: Meteor.userId()}); },
-	allCompleted: function(){ Items.update({checked:false, owner: Meteor.userId()}, {$set: {checked: true}}, {multi: true}); }
-	});
+	allCompleted: function(){ Items.update({checked:false, owner: Meteor.userId()}, {$set: {checked: true}}, {multi: true}); },
+	updateItem: function (id, checked) {Items.update(id, {$set: {checked: checked}})},
+	removeItem: function (id) {Items.remove(id)},
+	addItem: function(text){ Items.insert({text: text, createdAt: new Date(), checked: false, owner: Meteor.userId()});}
+});
 
 if (Meteor.isClient) {
+	Meteor.subscribe("items");
 	Session.set("hide-completed", false);
   Template.body.helpers({
-    items_completed: function(){
-    	return Items.find({checked: true, owner: Meteor.userId()}, {sort: {createdAt: -1}});
-    },
-    items_uncompleted: function(){
-    	if(!Meteor.user()){return Items.find({owner: '-1'})}
-    	return Items.find({checked: false, owner: Meteor.userId()}, {sort: {createdAt: -1}});
-    },
+    items_completed: function(){return Items.find({checked: true, owner: Meteor.userId()}, {sort: {createdAt: -1}});},
+    items_uncompleted: function(){return Items.find({checked: false, owner: Meteor.userId()}, {sort: {createdAt: -1}});},
     items_uncompleted_count: function(){return Items.find({checked: false, owner: Meteor.userId()}).count()},
-    items_uncompleted_count_is_not_zero: function(){if(!Meteor.user()){return false;} return Items.find({checked: false, owner: Meteor.userId()}).count() != 0},
+    items_uncompleted_count_is_not_zero: function(){Items.find({checked: false, owner: Meteor.userId()}).count() != 0},
     show_completed: function(){return !Session.get("hide-completed") }
   });
 
   Template.body.events({
     "submit .new-item" : function(e) {
-    	Items.insert({text: e.target.text.value, createdAt: new Date(), checked: false, owner: Meteor.userId()});
+    	Meteor.call("addItem", e.target.text.value)
     	e.target.text.value = "";
     	return false;
     },
@@ -50,8 +49,12 @@ if (Meteor.isClient) {
   });
   
   Template.item.events({
-  	"click .toggle-checked" : function () {Items.update(this._id, {$set: {checked: !this.checked}})},
-  	"click .delete" : function () {Items.remove(this._id)}
+  	"click .toggle-checked" : function(){Meteor.call("updateItem", this._id, !this.checked)},
+  	"click .delete" : function(){Meteor.call("removeItem", this._id)}
   });
+}
+
+if (Meteor.isServer){
+	Meteor.publish("items", function(){return Items.find({owner: this.userId})})
 }
 
