11b0fd816c8a64e171884c5e058a3d547b198b1a
Daniel Ruswick <dan.ruswick@gmail.com>
2015-01-29 16:11:39 -0600

    added view for archive

--- application.js
+++ application.js
@@ -3,10 +3,23 @@ Items = new Mongo.Collection("items");
 
 Meteor.methods({
 	removeCompleted: function(){ Items.update({checked: true, owner: Meteor.userId(), archived: {$ne: true}}, {$set: {archived: true}}, {multi: true}); },
-	allCompleted: function(){ Items.update({checked:false, owner: Meteor.userId()}, {$set: {checked: true}}, {multi: true}); },
-	updateItem: function (id, checked) {Items.update(id, {$set: {checked: checked}})},
-	removeItem: function (id) {Items.update({id: id}, {$set: {archived: true}})},
+	allCompleted: function(){ Items.update({checked: false, owner: Meteor.userId()}, {$set: {checked: true}}, {multi: true}); },
+	updateItem: function(id, checked) {Items.update(id, {$set: {checked: checked}})},
+	removeItem: function(id) {Items.update(id, {$set: {archived: true}})},
+	unarchiveItem: function(id) {Items.update(id, {$set: {archived: false}})},
 	addItem: function(text){ Items.insert({text: text, createdAt: new Date(), checked: false, owner: Meteor.userId(), archived: false});}
+});
+
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.route('/', function(){
+	this.render('items_view');
+});
+
+Router.route('/archived', function(){
+	this.render('archived_view');
 });
 
 
--- client/client.js
+++ client/client.js
@@ -4,9 +4,14 @@ if (Meteor.isClient) {
   Template.items_view.helpers({
     items_completed: function(){return Items.find({checked: true, owner: Meteor.userId(), archived: {$ne: true}}, {sort: {createdAt: -1}});},
     items_uncompleted: function(){return Items.find({checked: false, owner: Meteor.userId(), archived: {$ne: true}}, {sort: {createdAt: -1}});},
+    items_archived: function(){return Items.find({owner: Meteor.userId(), archived: true});},
     items_uncompleted_count: function(){return Items.find({checked: false, owner: Meteor.userId(), archived: {$ne: true}}).count()},
     items_uncompleted_count_is_not_zero: function(){Items.find({checked: false, owner: Meteor.userId(), archived: {$ne: true}}).count() != 0},
     show_completed: function(){return !Session.get("hide-completed") }
+  });
+  
+  Template.archived_view.helpers({
+    items_archived: function(){return Items.find({owner: Meteor.userId(), archived: true});}
   });
 
   Template.items_view.events({
@@ -24,6 +29,7 @@ if (Meteor.isClient) {
     	Meteor.call('allCompleted');
     }
   });
+  
   
   Template.settings_view.events({
   	"click .settings" : function(e){
@@ -43,5 +49,8 @@ if (Meteor.isClient) {
   Template.item.events({
   	"click .toggle-checked" : function(){Meteor.call("updateItem", this._id, !this.checked)},
   	"click .delete" : function(){Meteor.call("removeItem", this._id)}
+  });
+   Template.item_archived.events({
+  	"click .unarchive" : function(){Meteor.call("unarchiveItem", this._id)}
   });
 }
